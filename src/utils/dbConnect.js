ec278ef6b1a9b7072ef537ed69669e29cfc898f4
hanklikesmatcha <hank.likes.matcha@gmail.com>
2024-06-25 20:48:57 +1200

    remove console.log for db connection

--- src/utils/dbConnect.js
+++ src/utils/dbConnect.js
@@ -28,7 +28,6 @@ async function dbConnect() {
     cached.promise = mongoose
       .connect(MONGODB_URI, opts)
       .then((mongoose) => {
-        console.log("Connected to MongoDB");
         return mongoose;
       })
       .catch((error) => {
