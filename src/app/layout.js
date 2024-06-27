4eb5f78afea874a8d3a41bad541a7f4cf1653b85
hanklikesmatcha <hank.likes.matcha@gmail.com>
2024-06-27 16:10:34 +1200

    try meta in different file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,6 +12,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content="/og-image.jpg" />
+        <meta property="og:url" content="https://running4.life" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content="/ms-icon-144x144.png" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
