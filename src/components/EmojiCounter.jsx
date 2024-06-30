e4ef53ad29e8438c1d64dc4dc3381e58bd749a88
hanklikesmatcha <hank.likes.matcha@gmail.com>
2024-06-30 22:59:21 +1200

    update styling for emojicounter

--- src/components/EmojiCounter.jsx
+++ src/components/EmojiCounter.jsx
@@ -26,15 +26,15 @@ const EmojiCounter = ({ emoji, count, onClick }) => {
       className={`flex items-center justify-center rounded-xl border-0 border-purple-300 p-1 shadow-md ring-2 ring-pink-200 ring-offset-2 transition-transform sm:text-lg md:text-xl ${isAnimating ? "emoji-bounce" : ""}`}
       disabled={isAnimating}>
       <span
-        className={`mx-1 text-xs text-black sm:ml-2 sm:text-sm md:text-base ${isAnimating ? "emoji-pulse" : ""}`}>
-        {count}
-      </span>
-      <span
         className={`-m-2 p-2 text-base ${isAnimating ? "emoji-bounce" : ""}`}>
         {emoji}
       </span>
-      <span className="mx-1 text-xs text-black sm:ml-2 sm:text-sm md:text-base">
+      <span className="ml-1 text-xs text-black sm:ml-2 sm:text-sm md:text-base">
         {label}
+      </span>
+      <span
+        className={`ml-1 text-xs text-black sm:ml-2 sm:text-sm md:text-base ${isAnimating ? "emoji-pulse" : ""}`}>
+        {count}
       </span>
     </button>
   );
