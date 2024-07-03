bdabd433b06fd58e7958b734394a863646ecd792
hanklikesmatcha <hank.likes.matcha@gmail.com>
2024-07-03 16:52:53 +1200

    add aws sdk

--- src/app/api/feedback/route.js
+++ src/app/api/feedback/route.js
@@ -1,5 +1,5 @@
-import { Resend } from "resend";
 import { EmailTemplate } from "@/components/EmailTemplate.jsx";
+import { Resend } from "resend";
 export const revalidate = 0;
 
 const resend = new Resend(process.env.RESEND_API_KEY);
