563c91ca2e25f8ec37245baa25b6d89a4546430e
hanklikesmatcha <hank.likes.matcha@gmail.com>
2024-07-04 21:01:29 +1200

    add domain for route patterns

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,13 @@ const nextConfig = {
         protocol: "https",
         hostname: "running4life-public.s3.ap-southeast-2.amazonaws.com",
         pathname: "/**"
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**"
       }
     ]
   }
 };
-
 export default nextConfig;
